3238c1d7ce053211ab7eb0c51a9b4fefbcf6c28c
johnny <johnny_liao@johnny-liao-rd2>
2017-04-14 14:21:18 +0800

    modify category_member sidebar css

--- resources/views/demo/product_list.blade.php
+++ resources/views/demo/product_list.blade.php
@@ -2,33 +2,22 @@
 
 
 @section('content')
+<!--側欄CSS，借放-->
+<style>
+    .side li:hover {
+        background: #FFE100 !important;
+    }
+</style>
 
     <h1>D-Mail__test__所有信件清單--測試</h1>
 
     <!--側邊欄-->
     <div id="sidebar" style="float:left; width:200px; margin:20px;">
-        <table class="table table-striped table-hover">
-            <caption>Category</caption>
-            <thead>
-            <tr>
-                <!--<th>[P-key] recno</th>-->
-                <!--<th>c_no</th>-->
-                <th>name</th>
-                <!--<th>memo</th>-->
-            </tr>
-            </thead>
-            <tbody>
+        <ul class="side" style="list-style-type: none;">
             @foreach($categories as $category)
-                <tr>
-                <!--<td>{{$category->recno}}</td>-->
-                <!--<td>{{$category->c_no}}</td>-->
-                    <td>{{$category->name}}</td>
-                <!--<td>{{$category->memo}}</td>-->
-                </tr>
+                <li>{{$category->name}}</li>
             @endforeach
-            </tbody>
-            </tbody>
-        </table>
+        </ul>
     </div>
 
     <!--Products-->
