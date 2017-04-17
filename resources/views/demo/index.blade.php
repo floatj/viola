90084e94a4cbab205b08c1b76a04869dbde228ed
FloatJ <floatj@gmail.com>
2017-04-18 02:46:00 +0800

    index info modify blade template variable, to avoid escaping html tag

--- resources/views/demo/index.blade.php
+++ resources/views/demo/index.blade.php
@@ -5,7 +5,7 @@
     <h1>信件系統22222--測試頁面</h1>
     <h2>測試blalalalaalala</h2>
     <p>{{$supplier[0]->name}}</p>
-    <p>{{$supplier[0]->memo}}</p>
+    <p>{!!$supplier[0]->memo!!}</p>
     <p>{{$supplier[0]->addr_road}}</p>
     <p>{{$supplier[0]->tel}}</p>
 @stop
