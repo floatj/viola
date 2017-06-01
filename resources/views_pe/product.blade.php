215f46b7151e233cef19d12906ebb0aadf2edcfa
floatj <floatj@gmail.com>
2017-06-02 02:37:56 +0800

    產品介紹 表格 CSS 背景色 / Bootstrap responsive 修正方案
    - 覆寫表格背景色(如果沒有背景色的話)，覆寫表格 bootstrap css (class="table") 以使表格 responsive 行為正確
    - 如果 產品介紹撈出來的 html 的 table 內的 td 沒有背景色 套用 attr bgcolor=#FFF (空白底色)
    - 走訪表格所有 td ，如果該 td 沒有設定 bgcolor ，則強制設定 bgcolor 為 #FFF
    - 檢查 html 如有 表格 也套用 bootstrap css (移除 width 屬性, 增加 class="table" 屬性)

--- resources/views_pe/product.blade.php
+++ resources/views_pe/product.blade.php
@@ -33,8 +33,37 @@
         
 
     <div id="pro_title" class="row">產品詳細介紹</div>
-    <div class="table-responsive">
+    <div class="table-responsive" id="product_intro">
         {!! $product->memo !!}
     </div>
   <div id="news_btn" class="row"><a href="/products" class="btn_gray">Back</a></div>
 @stop
+
+@section('js')
+<script>
+
+    $(document).ready(function(){
+	/* 覆寫表格背景色(如果沒有背景色的話)，覆寫表格 bootstrap css (class="table") 以使表格 responsive 行為正確 */
+
+        //如果 產品介紹撈出來的 html 的 table 內的 td 沒有背景色 套用 attr bgcolor=#FFF (空白底色)
+    	//@TODO: 這好像有 Bug 無法完全 cover 到，先暫時拔掉
+	/*
+	if(! $('div#product_intro table td').attr('bgcolor'))
+          $('div#product_intro table td').attr('bgcolor', '#FFF');
+	*/
+
+
+	//為了解決上面的Bug...
+	//走訪表格所有 td ，如果該 td 沒有設定 bgcolor ，則強制設定 bgcolor 為 #FFF
+	$("td").each(function() {
+	    var bgcolor = $(this).attr("bgcolor");
+	    if(typeof bgcolor == "undefined") $(this).attr('bgcolor', '#FFF');
+	});	
+
+        //產品介紹表格內的 html 如有 表格 也套用 bootstrap css (移除 width 屬性, 增加 class="table" 屬性)
+        $('div#product_intro table').removeAttr('width');
+        $('div#product_intro table').attr('class', 'table');
+    });
+</script>
+@stop
+
