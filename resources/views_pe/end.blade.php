9379cfc5bae1e9fc21be8155e4147131461078d1
FloatJ Liao <floatj@gmail.com>
2017-07-19 02:22:00 +0800

    customer info modification

--- resources/views_pe/end.blade.php
+++ resources/views_pe/end.blade.php
@@ -1,6 +1,6 @@
 <div class="footer">
     <p>
-        <span style="color:#fff;">協和塑膠有限公司</span> © Copyright All Rights Reserved.<br>TEL：886-4-763-5792　FAX：886-4-763-5846　<br> 地址：台灣台中市太平區新坪里育仁路47巷7號<br>Site by <a href="http://tw.bysources.com" target="_blank">BySources</a>
+        <span style="color:#fff;">協和塑膠有限公司</span> © Copyright All Rights Reserved.<br>TEL：886-4-2391-0110　FAX：886-4-2391-4179　<br> 地址：台灣台中市太平區新坪里育仁路47巷7號<br>Site by <a href="http://tw.bysources.com" target="_blank">BySources</a>
         　<a href="http://tw.bysources.com/supply/login.php" target="_blank" id="admin_link">後台管理</a>
     </p>
 </div>
